a6c67c97e4b6a5129c4bec5f8da20018669c14e8
visua <visua@example.com>
2026-05-08 12:34:21 -0300

    Updates

---
+++ assets/upload-4XtGL7Bg.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-iLINJW9q.js";var t=e(`arrow-left`,[[`path`,{d:`m12 19-7-7 7-7`,key:`1l729n`}],[`path`,{d:`M19 12H5`,key:`x3x0zl`}]]),n=e(`upload`,[[`path`,{d:`M12 3v12`,key:`1x0j5s`}],[`path`,{d:`m17 8-5-5-5 5`,key:`7q97r8`}],[`path`,{d:`M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4`,key:`ih7n3h`}]]);export{t as n,n as t};
